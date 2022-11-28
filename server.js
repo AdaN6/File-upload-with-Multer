d138ba552d1ee41a702f6ca1e6bf7558f9883db9
AdaN6 <a_disney_da@hotmail.com>
2022-11-28 20:24:38 +0100

    added static express

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const server = express();
-
+const multer = require('multer');
 const port = process.env.PORT || 4000;
 
+
+const storage = multer.diskStorage({
+    desitnation: './uploads',
+    filename: (req, file, cb) => {
+        cb(null, `${file.originalname}-${Date.now()}`)
+    },
+});
+
+const upload = multer({storage: storage});
+
+server.use(express.static('views'));
+
+
+server.post("/upload-profile-pic", upload.single('profile_pic'), (req, res) => {
+res.send('You posted to /upload-profile-pic ');
+})
 
 
 server.listen(port, () => console.log(`Connected to port ${port}`))
