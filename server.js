d16dd9f771b630d97b0ad8831d2940eedfd13a1b
AdaN6 <a_disney_da@hotmail.com>
2022-11-28 22:12:48 +0100

    separated the module inside a lib folder

--- server.js
+++ server.js
@@ -1,28 +1,19 @@
 const express = require('express')
 const server = express();
-const multer = require('multer');
 const port = process.env.PORT || 4000;
-const path = require("path");
-
-
-// you need to add an extension * remember to import path, otherwise wont work!
-const storage = multer.diskStorage({
-  desitnation: "uploads/",
-  filename: (req, file, cb) => {
-    const extension = path.extname(file.originalname);
-    cb(null, `${file.fieldname}-${Date.now()}${extension}`);
-  },
-});
+const multer = require("multer");
+const {storage} = require('./lib/multerConfig')
 
 
 const upload = multer({storage: storage});
 
 server.use(express.static('views'));
+server.use(express.static('.'));
 
 server.post('/upload-profile-pic', upload.single('profile_pic'), (req, res) => {
     console.log(req.file);
-    return res.send('You posted to /upload-profile-pic ');
-    
+    if (!req.file) return res.status(400).send('Please upload a file');
+    return res.status(200).send(`<h2>Here is the picture:</h2><img src="${req.file.path}" alt="something" />`);
 })
 
 
