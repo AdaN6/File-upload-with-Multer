df9fde318de17061a188077efca49c7ca2a03511
AdaN6 <a_disney_da@hotmail.com>
2022-11-28 22:55:10 +0100

    throw error

--- server.js
+++ server.js
@@ -2,18 +2,27 @@ const express = require('express')
 const server = express();
 const port = process.env.PORT || 4000;
 const multer = require("multer");
-const {storage} = require('./lib/multerConfig')
+const {upload} = require('./lib/multerConfig')
 
 
-const upload = multer({storage: storage});
+
 
 server.use(express.static('views'));
 server.use(express.static('.'));
 
 server.post('/upload-profile-pic', upload.single('profile_pic'), (req, res) => {
     console.log(req.file);
-    if (!req.file) return res.status(400).send('Please upload a file');
+
+    if (!req.file) 
+    // return res.status(400).send("Please upload a file");
+    throw new Error('Please upload an image');
+
     return res.status(200).send(`<h2>Here is the picture:</h2><img src="${req.file.path}" alt="something" />`);
+})
+
+// error handling middleware you always need 4 parameters. (need to pass with the following structure err, req, res and next )
+server.use((err, req, res, next) => {
+    return res.status(500).send(`<h2>${err.message}</h2>`)
 })
 
 
