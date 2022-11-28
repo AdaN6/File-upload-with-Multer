aa34c1f02afce99d2590c90b788836a8b6bab3ef
AdaN6 <a_disney_da@hotmail.com>
2022-11-28 21:34:32 +0100

    added path and extension for the end of filename

--- server.js
+++ server.js
@@ -2,22 +2,27 @@ const express = require('express')
 const server = express();
 const multer = require('multer');
 const port = process.env.PORT || 4000;
+const path = require("path");
 
 
+// you need to add an extension * remember to import path, otherwise wont work!
 const storage = multer.diskStorage({
-    desitnation: './uploads',
-    filename: (req, file, cb) => {
-        cb(null, `${file.originalname}-${Date.now()}`)
-    },
+  desitnation: "uploads/",
+  filename: (req, file, cb) => {
+    const extension = path.extname(file.originalname);
+    cb(null, `${file.fieldname}-${Date.now()}${extension}`);
+  },
 });
+
 
 const upload = multer({storage: storage});
 
 server.use(express.static('views'));
 
-
-server.post("/upload-profile-pic", upload.single('profile_pic'), (req, res) => {
-res.send('You posted to /upload-profile-pic ');
+server.post('/upload-profile-pic', upload.single('profile_pic'), (req, res) => {
+    console.log(req.file);
+    return res.send('You posted to /upload-profile-pic ');
+    
 })
 
 
