df851ca400b1e56fbc2649d8e7fac99d18c8f7bc
AdaN6 <a_disney_da@hotmail.com>
2022-11-28 23:22:09 +0100

    map the uploaded multiple images

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const port = process.env.PORT || 4000;
 const multer = require("multer");
 const {upload} = require('./lib/multerConfig')
 
-
-
-
 server.use(express.static('views'));
 server.use(express.static('.'));
 
@@ -20,8 +17,13 @@ server.post('/upload-profile-pic', upload.single('profile_pic'), (req, res) => {
     return res.status(200).send(`<h2>Here is the picture:</h2><img src="${req.file.path}" alt="something" />`);
 })
 
-server.post('/upload-cat-pic', (req, res) => {
-    
+server.post('/upload-cat-pics', upload.array('cat_pics', 5), (req, res) => {
+    console.log(req.files)
+    if (!req.files) return res.status(400).send(`Please upload many cat pics!`);
+    const allCatPics = req.files.map(
+      (image) => `<div> <img src="${image.filename}" alt="something" /></div>`
+    ).join('');
+ return res.status(200).send(`<h2>here are your pics: </h2>${allCatPics}`)
 })
 
 // error handling middleware you always need 4 parameters. (need to pass with the following structure err, req, res and next )
